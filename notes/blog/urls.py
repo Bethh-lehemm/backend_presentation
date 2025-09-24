f8ef9dbcaa868b3993a4bb6c746e85bdbe1697c7
Bethh-lehemm <peterbeth374@gmail.com>
2025-09-24 10:47:38 +0100

    Rennovating git repository

--- notes/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import BloglistView 
-
-urlpatterns = [
-    path('', BloglistView.as_view(), name='home'),
-    path('home.html', BloglistView.as_view(), name='home'),
-    path('notes.html', BloglistView.as_view(), name='notes'),
-]
